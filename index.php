41ecf28bfb68d790088dc70b9befb510632f4cf5
Moyza <moysesbrito51@gmail.com>
2025-03-11 18:19:11 -0300

    Login

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
-           <div class =  "<col-lg-4 offset-lg-4">
-                <form action="login.php" method = "Post">
+            <div class="<col-lg-4 offset-lg-4">
+                <form action="login.php" method="Post">
                     <div>
                         <div class="mb-3">
                             <label>Usuário</label>
-                            <input type="text"
-                            name="usuario"
-                            class="form-control">
+                            <input type="text" name="usuario" class="form-control">
                         </div>
                     </div>
                     <div>
                         <div class="mb-3">
-                        <label>Senha</label>
-                            <input type="passaword"
-                            name="senha"
-                            class="form-control">
+                            <label>Senha</label>
+                            <input type="password" name="senha" class="form-control">
                         </div>
                     </div>
                     <div>
                         <div class="mb-3">
-                            <button type="submit"
-                            class= "btn btn-primary">Enviar</button>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
                         </div>
                     </div>
-           </div>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
